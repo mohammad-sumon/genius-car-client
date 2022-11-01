c054874bed1a8e2315f0f7d65d1a0d2e377f2636
mohammad-sumon <sumon3011@gmail.com>
2022-11-02 00:14:43 +0600

    module 67.3 added

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,4 +1,5 @@
 import Main from "../../Layout/Main";
+import Checkout from "../../Pages/Checkout/Checkout";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import SignUp from "../../Pages/SignUp/SignUp";
@@ -6,24 +7,30 @@ import SignUp from "../../Pages/SignUp/SignUp";
 const { createBrowserRouter } = require("react-router-dom");
 
 const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Main></Main>, 
-      children: [
-        {
-            path: '/',
-            element: <Home></Home>
-        },
-        {
-          path: '/login', 
-          element: <Login></Login>
-        },
-        {
-          path: '/signup', 
-          element: <SignUp></SignUp>
-        },
-      ]
-    }
-  ]);
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/signup",
+        element: <SignUp></SignUp>,
+      },
+      {
+        path: "/checkout/:id",
+        element: <Checkout></Checkout>,
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/services/${params.id}`),
+      },
+    ],
+  },
+]);
 
-  export default router;
+export default router;
